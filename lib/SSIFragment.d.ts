47d0b136c0a3afd2eb592df0bdd8211634577d7b
Dibran Isufi <d.isufi@gmail.com>
2020-10-02 14:45:56 +0200

    fix: temporarily add build artifacts

---
+++ lib/SSIFragment.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import { SSIFragmentProps } from './types';
+export declare const SSIFragment: (props: SSIFragmentProps) => JSX.Element;
---
+++ lib/types.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+export interface SSIFragmentProps {
+    isOnClient?: boolean;
+    id: string;
+    url: string;
+    onReady?: () => void;
+}
+export declare const SSIFragment: React.FunctionComponent<SSIFragmentProps>;
